bc11d4a7cfee9a9dae665f2b290f670ec47a8b6d
vismayakd <vismayakd1993@gmail.com>
2025-11-06 19:37:32 +0530

    Added .gitignore and updated settings for env variables

--- ServiceConnect/settings.py
+++ ServiceConnect/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,8 +141,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'vismayakd1993@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpet mjzn karz xmjr'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-secondary',
