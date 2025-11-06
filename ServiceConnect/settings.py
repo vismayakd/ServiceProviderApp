2d91f11d5b9b4812b3f28bd67acbb22a1209ed16
vismayakd <vismayakd1993@gmail.com>
2025-11-06 22:28:47 +0530

    Fix SECRET_KEY and email environment variable usage

--- ServiceConnect/settings.py
+++ ServiceConnect/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#qjypkh+*7gn@baqio*x5g*0b+#jlrct)t^9$^&ay2^j2c3-c'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
